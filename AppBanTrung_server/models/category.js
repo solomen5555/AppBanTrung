305eca0fac3834514875a21d50eeda4e2a174b5b
= <=>
2021-09-11 07:52:31 +0700

    thêm phân loại api

--- AppBanTrung_server/models/category.js
+++ AppBanTrung_server/models/category.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-    
+    Ten:{
+        type:String,
+        required:true
+    },
+    Icon:{
+        type:String,
+    },
+    Color:{
+        type:String,
+    }
 })
 
 exports.Category = mongoose.model('Category',categorySchema);
--- AppBanTrung_server/routes/categories.js
+++ AppBanTrung_server/routes/categories.js
@@ -12,6 +12,22 @@ router.get(`/`,async (req,res)=>{
     res.send(categoryList);
 })
 
+router.post(`/`,async (req,res)=>{
+    const category = new Category({
+        Ten: req.body.Ten,
+        Icon: req.body.Icon,
+        Color:req.body.Color
+    })
+
+    category = await category.save();
+    if(!category){
+        return res.status(404).send('Thêm thất bại!');
+    }
+
+    res.send(category);
+   
+    
+})
 
 
 module.exports = router;
