d3a0bc9d2e1b1ce5d1cf7c3a2d0c9cae0d33d576
= <=>
2021-09-11 07:52:50 +0700

    thêm product api

--- AppBanTrung_server/models/product.js
+++ AppBanTrung_server/models/product.js
@@ -2,11 +2,59 @@ const mongoose = require('mongoose');
 
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image:String,
-    countInStock:{
+    Ten: {
+        type: String,
+        required: true
+    },
+    MoTa:{
+        type: String,
+        required:true
+    },
+    MoTaChiTiet:{
+        type:String,
+        default:''
+    },
+    Anh: {
+        type: String,
+        default:''
+    },
+    BoAnh:[{
+        type:String
+    }],
+    ThuongHieu:{
+        type:String,
+        default:''
+    },
+    Gia:{
         type:Number,
-       required:true
+        default:0
+    },
+    Loai:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+    },
+    TonKho:{
+    type:Number,
+       required:true,
+       min :0,
+       max:255
+    },
+    XepHang:{
+        type:Number,
+        default:0
+    },
+    LuotDanhGia:{
+        type:Number,
+        default:0
+    },
+    AnTuong:{
+        type:Boolean,
+        default:false
+    },
+    NgayTao:{
+        type:Date,
+        default:Date.now
     }
 })
 
--- AppBanTrung_server/routes/products.js
+++ AppBanTrung_server/routes/products.js
@@ -12,20 +12,20 @@ router.get(`/`,async (req,res)=>{
     res.send(productList);
 })
 
-router.post(`/`,(req,res)=>{
-    const product = new Product({
-        name :req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    product.save().then((createdProduct=>{
-        res.status(201).json(createdProduct)    
-    })).catch((err)=>{
-        res.status(500).json({
-            error : err,
-            success: false
-        })
-    })
-})
+// router.post(`/`,(req,res)=>{
+//     const product = new Product({
+//         name :req.body.name,
+//         image: req.body.image,
+//         countInStock: req.body.countInStock
+//     })
+//     product.save().then((createdProduct=>{
+//         res.status(201).json(createdProduct)    
+//     })).catch((err)=>{
+//         res.status(500).json({
+//             error : err,
+//             success: false
+//         })
+//     })
+// })
 
 module.exports = router;
